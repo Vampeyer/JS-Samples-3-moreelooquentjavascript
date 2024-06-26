0f72f892a2ae6106470d79e6ea55e1597f93835e
unknown <jthompson0376official@gmail.com>
2024-06-26 15:45:28 -0700

    Picking back up on ch6  ...

---
+++ ch5&6/classes/basicclass.js
@@ -0,0 +1,17 @@
+
+class Rabbit {
+    constructor(type) {
+      this.type = type;
+    }
+    speak(line) {
+      console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+  }
+let rabbiter = new Rabbit("blue and crazy ")
+
+rabbiter.speak("Wazza zaaap ! s")
+
+// Now from the book  
+
+let killerRabbit = new Rabbit(killer); 
+
